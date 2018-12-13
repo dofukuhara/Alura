55918379475ce59ea500bc36a84231269210210a
Douglas Fukuhara <dofukuhara@gmail.com>
2018-12-13 08:27:17 -0200

    [FinancasK] Using Lambda Expressions in Object Expression
     - Using Lambda Expression to implement the onClickListener
       interface along with Object Expression
     - By doing so, the code was simplified and now 'this'
       is referring to the calling function scope, and not to
       the anonimous function scope.

--- FinancasK/app/src/main/java/br/com/dofukuhara/financask/ui/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/br/com/dofukuhara/financask/ui/activity/ListaTransacoesActivity.kt
@@ -32,21 +32,26 @@ class ListaTransacoesActivity : AppCompatActivity() {
             Em Kotlin não temos a 'keywork' new para instânciar um objeto. Nesse caso, utilizamos a
             keywork object com o intuito de informar que vamos passar um objeto e explicitamos o seu
             tipo com o caracter :
-            
+
             Note que, similar como em Java, quando implementamos funções anônimas, a referência ao 'this'
             é do escopo da função implementada (View) e não da Activity a qual estamos definindo.
             Para isso, caso queria referenciar o this para essa Activity (ou para qualquer outra classe),
-            podemos utilizar uma 'label' para fazer essa refência
+            podemos utilizar uma 'label' para fazer essa refência (this@ListaTransacoesActivity)
         */
-        lista_transacoes_adiciona_receita.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                Toast.makeText(
-                        this@ListaTransacoesActivity,
-                        "Clique em nova receita",
-                        Toast.LENGTH_SHORT).show()
-            }
+        /*
+            Como sugerido pelo Studio, além de implementar a interface utilizando Object Expression,
+            podemos também implementá-lo utilizando lambda expression.
 
-        })
+            IMPORTANTE: Note que a implementação ficou 'menos verbosa' e que, ao utilizar lambda expression,
+            o escopo da função é elevada ao nível de quem a chama, então nesse caso o this passa a se
+            referenciar à nossa Activity, podendo assim remover o label "@ListaTransacoesActivity" de this.
+         */
+        lista_transacoes_adiciona_receita.setOnClickListener {
+            Toast.makeText(
+                    this,
+                    "Clique em nova receita",
+                    Toast.LENGTH_SHORT).show()
+        }
 
     }
 
