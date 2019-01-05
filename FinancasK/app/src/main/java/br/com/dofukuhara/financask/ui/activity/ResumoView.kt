43cf231f30eba981279ad4e1cd241109787695e0
Douglas Fukuhara <dofukuhara@gmail.com>
2019-01-05 10:43:29 -0200

    [FinancasK] Finished Class #2
      - Some strategies to handle Null Cases:
      	= Null check with IF statement
    	= Safe Call operator
    	= Safe Call operator and 'let {...}'
    
    Signed-off-by: Douglas Fukuhara <dofukuhara@gmail.com>

--- FinancasK/app/src/main/java/br/com/dofukuhara/financask/ui/activity/ResumoView.kt
+++ FinancasK/app/src/main/java/br/com/dofukuhara/financask/ui/activity/ResumoView.kt
@@ -32,21 +32,28 @@ class ResumoView (context : Context,
         val totalReceitas = resumo.receita
 
         /*
-            Na assinatura do construtor dessa classe, foi ajustado para que o tipo de view seja
-            "View?", ou seja, a variável passada como parâmetro pode ser nula.
-            Então, em todas as chamadas a essa variável, o Kotlin irá indicar como possível ponto de
-            falha e não permitirá que o código compile.
-            Para isso, podemos utilizar o operador !! => non-null asserted(!!)
-            Ao utilizarmos esse operador, informamos ao Kotlin que nós iremos nos responsibilizar de
-            que ela "nunca" seja nula e que permita assim a compilação do código.
+            Anteriormente utilizamos a abordagem de non-null asserted (!!) para informar ao Kotlin
+            que o código pode ser compilado pois garantiremos que a variável não será nula durante
+            o seu acesso.
+            Porém, essa não é uma boa abordagem para lidar com esse cenário.
 
-            Obs: Ao utilizar essa estratégia, caso a variável (com o !!), ao ser invocada tenha valor
-            nulo, ao invés de lançar um 'tradicional' NullPointerException, será lançado um erro
-            KotlinNullPointerException
+            1 das maneiras que podemos realizar é uma bem similar no Java, que é a de cercar seu
+            acesso através de um IF que verifique se a variável é nula, e somente se não for, acessar
+            seu conteúdo.
+
+            Obs: Note que neste caso, o Kotlin realizou um "Smart Cast" da variável 'view' para
+            "android.view.View" --> Isso ocorre pois como já fizemos a checagem antes de null, estamos
+            garantindo que a variável nunca será nula, então o Kotlin automaticamente faz o cast de
+            View? para View
+
+            Veja no método "adicionaDespesa()" outra abordagem para lidar com Null
         */
-        with (view!!.resumo_card_receita) {
-            setTextColor(corReceita)
-            text = totalReceitas.formatToBrazilizan()
+
+        if (view != null) {
+            with (view.resumo_card_receita) {
+                setTextColor(corReceita)
+                text = totalReceitas.formatToBrazilizan()
+            }
         }
     }
 
@@ -54,10 +61,24 @@ class ResumoView (context : Context,
     private fun adicionaDespesa() {
 
         val totalDespesas = resumo.despesa
-        with(view!!.resumo_card_despesa) {
-            setTextColor(corDespesa)
-            text = totalDespesas.formatToBrazilizan()
-        }
+
+        /*
+            Outra abordagem que podemos utilizar é a de utilizar o operador de "Safe Call" '?'
+            Ao utilizar esse operador, o Kotlin só irá executar/acessar o valor da variável se a
+            mesma não for nula.
+
+            Note que, no caso abaixo, fizemos a utilização de view? para acessar o 'resumo_card_despesa',
+            porém, como o 'resumo_card_despesa' também pode ser nulo, devemos também fazer uma "chamada
+            segura" dessa property.
+
+            Obs: Como o "with()" não aceita a utilização desse operador, tivemos que "sacrificar" seu
+            uso
+
+            Veja no método "adicionaTotal()" outra abordagem para lidar com Null
+        */
+
+        view?.resumo_card_despesa?.setTextColor(corDespesa)
+        view?.resumo_card_despesa?.text = totalDespesas.formatToBrazilizan()
     }
 
     private fun adicionaTotal() {
@@ -65,25 +86,28 @@ class ResumoView (context : Context,
         val resumoTotal = resumo.total
         val cor = corPor(resumoTotal)
 
-        with(view!!.resumo_card_total) {
-            setTextColor(cor)
-            text = resumoTotal.formatToBrazilizan()
+        /*
+            Uma abordagem bastante utilizada no mundo Kotlin é a de utilizar a função 'let {...}'
+            Essa função executa seu block (lambda) utilizando o objeto chamador como parâmetro e
+            retorna o resultado.
+            Note que estamos utilizando também a "Safe Call" da variável com a let{}. Dessa forma,
+            instruímos ao Kotlin que o block de let só será executado se a variável não for nula!
+            Similar à primeira abordagem, como garantimos que a variável não será nula, o Kotlin já
+            realiza um Smart Cast de View? para android.view.View
+        */
+        view?.let {
+            /*
+                Porém, como estamos dentro do block da 'let', podemos trocar a chamada da variável
+                "view" pelo seu objeto subentendido 'it'
+            */
+            with(it.resumo_card_total) {
+                setTextColor(cor)
+                text = resumoTotal.formatToBrazilizan()
+            }
         }
     }
 
     private fun corPor(valor: BigDecimal): Int {
-
-        /*
-
-            valor.compareTo(BigDecimal.ZERO)
-
-         */
-        /*
-            Diferente do mundo Java, o Kotlin não possui tipos (obj) primitivos.
-            Logo, para comparar objetos considerados em Java como "não primitivos", não precisamos
-            utilizar o 'compareTo()' ou o 'equals()'... podemos simplismente utilizar os operadores
-            lógicos, por exemplo, >= ou ==, pois no Kotlin, tudo acaba sendo comparação de objetos.
-         */
 
         if (valor >= BigDecimal.ZERO) {
             return corReceita
