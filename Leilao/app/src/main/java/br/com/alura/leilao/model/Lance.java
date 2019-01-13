71cb3648d5aec8d28580a740c90cb7d840792d0f
Douglas Fukuhara <dofukuhara@gmail.com>
2019-01-13 21:01:10 -0200

    [AutoTest][Leilao] Finished Class #1
      - Initial implementation of Leilao features:
        - Get higher and lower bids from the Auction
        - Show the higher bid value in Auction List
        - Display the Higher and the Lower bid values
          in the Auction Details screen
    
    Signed-off-by: Douglas Fukuhara <dofukuhara@gmail.com>

--- Leilao/app/src/main/java/br/com/alura/leilao/model/Lance.java
+++ Leilao/app/src/main/java/br/com/alura/leilao/model/Lance.java
@@ -12,4 +12,8 @@ public class Lance implements Serializable {
         this.valor = valor;
     }
 
+    public double getMaiorValor() {
+        return this.valor;
+    }
+
 }
--- Leilao/app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ Leilao/app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -8,14 +8,34 @@ public class Leilao implements Serializable {
 
     private final String descricao;
     private final List<Lance> lances;
+    private double maiorLance = Double.NEGATIVE_INFINITY;
+    private double menorLance = Double.POSITIVE_INFINITY;
 
     public Leilao(String descricao) {
         this.descricao = descricao;
         this.lances = new ArrayList<>();
     }
 
+    public double getMaiorLance() {
+        return this.maiorLance;
+    }
+
+    public double getMenorLance() {
+        return this.menorLance;
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
+    public void propoe(Lance lance) {
+        double valorLance = lance.getMaiorValor();
+        if (valorLance > maiorLance) {
+            maiorLance = valorLance;
+        }
+        
+        if (valorLance < menorLance) {
+            menorLance = valorLance;
+        }
+    }
 }
