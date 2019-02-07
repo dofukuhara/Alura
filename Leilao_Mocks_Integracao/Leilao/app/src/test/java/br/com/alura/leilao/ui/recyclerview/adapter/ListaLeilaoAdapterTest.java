65bf9592a212f878f2fc6728b2890f7ea9327168
Douglas Fukuhara <dofukuhara@gmail.com>
2019-02-06 23:11:06 -0200

    [AutoTest][Leilao][Part3] Finished Class $3
      - Handling Assynchrounous code in Unit Test
      - Handling the execution of interfaces that where passed as an
      argument from mocked methods
      - Refactor Source Code so that it could allow to make use of
      mocks in Unit Test
      - Simulate Integration Environment
    
    Signed-off-by: Douglas Fukuhara <dofukuhara@gmail.com>

--- Leilao_Mocks_Integracao/Leilao/app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ Leilao_Mocks_Integracao/Leilao/app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -3,7 +3,11 @@ package br.com.alura.leilao.ui.recyclerview.adapter;
 import android.content.Context;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,30 +16,26 @@ import br.com.alura.leilao.model.Leilao;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoAdapterTest {
+
+    // Injeção de Dependência do Contexto e do objeto SPY de ListaLeilaoAdapter
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes() {
         /*
-            Como precisamos passar um contexto no construtor de ListaLeilaoAdapter, podemos criar um
-            a partir do "Mockito.mock()". Com isso, o Mockito irá criar um objeto a partir da refe-
-            rência de classe passada como parâmetro.
+            Como estamos executando os testes utilizando o runner "MockitoJUnitRunner", não será
+            necessário executar o initMocks.
          */
-        Context context = Mockito.mock(Context.class);
-        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
-        /*
-            Como o Mockito não consegue mockar métodos estáticos e final, não conseguimos 'espiar' se
-            o "notifyDataSetChanged()" (método final do RecyclerView [context]), encapsulamos essa
-            chamada em uma função pública "atualizaLista()" de ListaLeilaoAdapter e, a partir desda
-            função, espiamos o objeto e, ao ser invocada, passamos a não executar nenhuma ação,
-            através do método estático do Mockito "doNothing()".
+//        MockitoAnnotations.initMocks(this);
 
-            A diferença do Mockito.mock() e Mockito.spy() é que no spy, as funçoes da classe são de
-            fato invocadas, enquanto que na mock não (caso alguma função desse objeto mock seja
-            invocada, ela simplesmente não executará/retornará nada)
-         */
-        Mockito.doNothing().when(adapter).atualizaLista();
+        doNothing().when(adapter).atualizaLista();
 
         adapter.atualiza(new ArrayList<>(Arrays.asList(
                 new Leilao("Console"),
@@ -44,13 +44,7 @@ public class ListaLeilaoAdapterTest {
 
         int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
-        /*
-            Já que no início do teste, estamos falando para o Mockito que quando o método "atualizaLista()"
-            do objeto que ele está 'espiando' (ListaLeilaoAdapter), queremos garantir que esse método seja,
-            de fato, chamado dentro da função "getItemCount()". Para isso, podemos utilizar o método estátio
-            "verify()".
-         */
-        Mockito.verify(adapter).atualizaLista();
+        Mockito.<ListaLeilaoAdapter>verify(adapter).atualizaLista();
 
         assertThat(quantidadeLeiloesDevolvida, is(2));
     }
