285a582a4a3ef3614c890c2dfd62f23b9e9ce278
Douglas Fukuhara <dofukuhara@gmail.com>
2019-03-31 20:50:44 -0300

    [AndroidRoom] Finished Class #3
     - Initial handling case when an Entity needs to be updated (in this
       case, a new attribute is being added)

--- AndroidRoom/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ AndroidRoom/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -9,21 +9,15 @@ import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
 /*
-    Para o Room, somente anotar a classe com "@Database" não é o suficiente. Além disso, a classe
-    deve extender a classe "RoomDatabase".
-    Além disso, o Room recomenda que a classe que for extender "RoomDatabase" também seja declarada
-    como abstrata. Isso é feito para que não seja necessário implementar manualmente os métodos de
-    acesso ao Database, deixando essa responsabilidade para o Room.
+    Quando fazemos uma alteração na Entity (foi adicionado uma nova coluna 'Sobrenome'), devemos:
+        - Incrementar a versão do Database
  */
 
-@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
     private static final String NOME_BRANCO_DE_DADOS = "agenda.db";
 
-    /*
-                Método (abstrato, que o Room irá implementar) que retorna uma instância do nosso DAO.
-             */
     public abstract AlunoDAO getRoomAlunoDAO();
 
     public static AgendaDatabase getInstance(Context context) {
