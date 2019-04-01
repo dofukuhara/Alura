f3a2d7f90d3ee17fa14f4c9f34477939fe2b6a80
Douglas Fukuhara <dofukuhara@gmail.com>
2019-03-31 23:31:36 -0300

    [AndroidRoom] Class #3
     - Using Migration to update DB scheme

--- AndroidRoom/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ AndroidRoom/app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,20 +1,15 @@
 package br.com.alura.agenda.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
-
-/*
-    Quando fazemos uma alteração na Entity (foi adicionado uma nova coluna 'Sobrenome'), devemos:
-        - Incrementar a versão do Database
-        - Devemos definir um Migration ou utilizar o método 'fallbackToDestructiveMigration()'
-            - O 'fallbackToDestructiveMigration()' não é recomendado utilizar quando se está em prod
-            pois no caso de uma mudança no schema, todos os dados anteriores seram perdidos.
- */
 
 @Database(entities = {Aluno.class}, version = 2, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
@@ -23,11 +18,21 @@ public abstract class AgendaDatabase extends RoomDatabase {
 
     public abstract AlunoDAO getRoomAlunoDAO();
 
+    /*
+        Para realizar a migração da Database sem perder os dados, podemos utilizar o Migration.
+        Assim, definimos qual a instrução SQL que deve ser executada para que o Room consiga
+        atualizar o schema do DB a partir da versão anterior.
+     */
     public static AgendaDatabase getInstance(Context context) {
         return Room
                 .databaseBuilder(context, AgendaDatabase.class, NOME_BRANCO_DE_DADOS)
                 .allowMainThreadQueries()
-                .fallbackToDestructiveMigration()
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                    }
+                })
                 .build();
     }
 }
