8470fc98d8e8076ffd0bb207b83b9cd1a5017edb
Douglas Fukuhara <dofukuhara@gmail.com>
2019-05-26 20:46:07 -0300

    [AndroidWebService][Part2] Configure URL in gradle LocalProperties file

--- AndroidWebService_parte2/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
+++ AndroidWebService_parte2/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -1,5 +1,7 @@
 package br.com.alura.agenda.retrofit;
 
+import br.com.alura.agenda.BuildConfig;
+import br.com.alura.agenda.R;
 import br.com.alura.agenda.services.AlunoService;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -23,7 +25,7 @@ public class RetrofitInicializador {
         client.addInterceptor(interceptor);
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.85.217:8080/api/")
+                .baseUrl(BuildConfig.LOCAL_WEBSERVER_URL)
                 .addConverterFactory(JacksonConverterFactory.create())
                 .client(client.build())
                 .build();
