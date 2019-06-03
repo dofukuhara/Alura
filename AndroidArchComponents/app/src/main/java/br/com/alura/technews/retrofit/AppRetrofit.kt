cff9b2eddd6eaf8629ffb6644fbdb4b983725b81
Douglas Fukuhara <dofukuhara@gmail.com>
2019-06-03 08:30:03 -0300

    [AndroidArchComponent] Initial Project Commit

---
+++ AndroidArchComponents/app/src/main/java/br/com/alura/technews/retrofit/AppRetrofit.kt
@@ -0,0 +1,32 @@
+package br.com.alura.technews.retrofit
+
+import br.com.alura.technews.BuildConfig
+import br.com.alura.technews.retrofit.service.NoticiaService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = BuildConfig.LOCAL_WEBSERVER_URL
+
+class AppRetrofit {
+
+    private val client by lazy {
+        val interceptador = HttpLoggingInterceptor()
+        interceptador.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder()
+            .addInterceptor(interceptador)
+            .build()
+    }
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+    val noticiaService: NoticiaService by lazy {
+        retrofit.create(NoticiaService::class.java)
+    }
+
+}
